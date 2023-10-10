acc0e822fb6841b698d8dd45d193feab94e470fd
Jesper Dinger <78754860+Gweppi@users.noreply.github.com>
2023-10-10 08:28:18 +0000

    jonas vindt groen leuk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,42 @@
 <script lang="ts">
-    import { Card, Select, Label, Button } from 'flowbite-svelte';
+    import { Card, Select, Label, Button, Range } from 'flowbite-svelte';
 
     let selected: string;
 
     let countries = [
         { value: 'erg', name: 'Engels Raaigras' }
     ];
+
+    let ph: number = 7;
 </script>
 
-<Card class="">
-    <Label class="w-full">
-        Selecteer grassoort
-        <Select class="mt-2 w-full max-w-md" items={countries} bind:value={selected} />
-    </Label>
+<div class="flex w-full justify-center p-4">
+    <Card class="w-full">
+        <div class="flex gap-4 flex-col">
+            <Label class="w-full">
+                Selecteer grassoort
+                <Select class="mt-2 w-full max-w-md" items={countries} bind:value={selected} />
+            </Label>
 
-    <div class="w-full flex flex-row gap-2 max-w-md pt-2">
-        <Button class="w-full" color="alternative">Annuleer</Button>
-        <Button class="w-full" color="primary">Gereed</Button>
-    </div>
-</Card>
+            <Label for="default-input" class="w-full">
+                <p>pH waarde: {ph}</p>
+                <Range color="green" id="range-steps" min="0" max="14" bind:value={ph} step="1" />
+            </Label>
+    
+            <Label class="w-full">
+                Selecteer grassoort
+                <Select class="mt-2 w-full max-w-md" items={countries} bind:value={selected} />
+            </Label>
+    
+            <Label class="w-full">
+                Selecteer grassoort
+                <Select class="mt-2 w-full max-w-md" items={countries} bind:value={selected} />
+            </Label>
+        </div>
+    
+        <div class="w-full flex flex-row gap-2 max-w-md pt-10">
+            <Button class="w-full" color="alternative">Annuleer</Button>
+            <Button class="w-full" color="green">Gereed</Button>
+        </div>
+    </Card>
+</div>

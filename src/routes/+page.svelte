c92b43f4c7356c994e6171b7b4357eec89581c20
Gweppi <dev@jesperdinger.com>
2023-11-06 16:39:28 +0100

    new design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,42 @@
 <script lang="ts">
-    import { Card, Select, Label, Button, Range } from 'flowbite-svelte';
+	import CustomButton from "$lib/CustomButton.svelte";
 
-    let selected: string;
-
-    let countries = [
-        { value: 'erg', name: 'Engels Raaigras' }
-    ];
-
-    let ph: number = 7;
 </script>
 
-<div class="flex w-full justify-center p-4">
-    <Card class="w-full">
-        <div class="flex gap-4 flex-col">
-            <Label class="w-full">
-                Selecteer grassoort
-                <Select class="mt-2 w-full max-w-md" items={countries} bind:value={selected} />
-            </Label>
+<div class="flex h-full">
+    <div class="flex flex-col gap-20 ml-20 mt-20">
+        <div class="flex flex-col gap-10">
+            <p class="text-sm text-gray-400">Vraag 1</p>
+    
+            <h1 class="text-3xl font-bold">Als ik een vraag vraag,
+                antwoord dan de vraag?
+            </h1>
+    
+            <h2 class="font-light">Dit is zeer belangrijke informatie,
+                als we deze informatie niet hadden,
+                zouden we gaan huilen in een hoekje.
+                Of erger, schreeuwen van de geestelijke pijn.
+            </h2>
+        </div>
+    
+        <div class="flex flex-row gap-7">
+            <CustomButton text="ja" />
+            <CustomButton text="nee" />
+            <CustomButton text="boeie ruurt" />
+        </div>
+    
+        
+        <div class="flex w-full relative mt-auto mb-24">
+            <div class="z-1 bg-gray-200 h-2 w-[100%] rounded-full"></div>
+            <div class="absolute inset-0 z-2 bg-green-400 h-2 w-[20%] rounded-full"></div>
+        </div>
+    </div>
 
-            <Label for="default-input" class="w-full">
-                <p>pH waarde: {ph}</p>
-                <Range color="green" id="range-steps" min="0" max="14" bind:value={ph} step="1" />
-            </Label>
-    
-            <Label class="w-full">
-                Selecteer grassoort
-                <Select class="mt-2 w-full max-w-md" items={countries} bind:value={selected} />
-            </Label>
-    
-            <Label class="w-full">
-                Selecteer grassoort
-                <Select class="mt-2 w-full max-w-md" items={countries} bind:value={selected} />
-            </Label>
-        </div>
-    
-        <div class="w-full flex flex-row gap-2 max-w-md pt-10">
-            <Button class="w-full" color="alternative">Annuleer</Button>
-            <Button class="w-full" color="green">Gereed</Button>
-        </div>
-    </Card>
+    <div class="flex items-end justify-end w-full pointer-events-none">
+        <img class="max-h-screen" src="grass.png" alt="gras">
+    </div>
+</div>
+
+<div class="absolute bottom-1 left-[50%] text-gray-300 text-xs">
+    PWS 2023
 </div>
